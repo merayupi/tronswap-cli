7fe87b25515f4c143b6634b96502bca7d9243361
meracon <bintang555pamungkas@gmail.com>
2024-08-21 20:49:35 +0700

    add sunpumpRouter

--- index.js
+++ index.js
@@ -58,36 +58,47 @@ const saleToken = async (sunPadLaunchpadContract, tokenAddress, balanceToken) =>
 }
 
 (async()=>{
-    try {
-        const privateKey = process.env.PRIVATE_KEY;
-        const tronWeb = new TronWeb(fullNode, solidityNode, eventServer, privateKey);
-        const address = tronWeb.address.fromPrivateKey(privateKey);
-        const balanceInSun = await tronWeb.trx.getBalance(address);
-
-        const implementationABI  = JSON.parse(fs.readFileSync('abiSunpad.json', 'utf8'));
-        const tokenContractABI  = JSON.parse(fs.readFileSync('abitoken.json', 'utf8'));
-        const sunPadLaunchpadCa = 'TTfvyrAz86hbZk5iDpKD78pqLGgi8C7AAw'
-        
-        let sunPadLaunchpadContract = await tronWeb.contract(implementationABI, sunPadLaunchpadCa);
-        
-        console.log(`Address: ${address}\nBalance: ${Number(balanceInSun)/10**6} TRX`);
-        console.log('1. Swap TRX to Token\n2. Sale Token to TRX');
-        const choice = rl.question('Choice: ');
-
-        if(choice == 1){
-            const tokenAddress = rl.question('Token Address: ');
-            const trxAmount = Number(rl.question('TRX Amount: '));
-            await buyTokenInSunPump(sunPadLaunchpadContract, tokenAddress, (trxAmount*10**6));
-        }else if(choice == 2){
-            const tokenAddress = rl.question('Token Address: ');
-            const tokenContract = await tronWeb.contract(tokenContractABI, tokenAddress);
-            const balanceToken = await getBalance(tokenContract, address);
-            console.log(`Balance Token: ${Number(balanceToken)/10**16}`);
+    while(true){
+        try {
+            const privateKey = process.env.PRIVATE_KEY;
+            const tronWeb = new TronWeb(fullNode, solidityNode, eventServer, privateKey);
+            const address = tronWeb.address.fromPrivateKey(privateKey);
+            const balanceInSun = await tronWeb.trx.getBalance(address);
+    
+            const implementationABI  = JSON.parse(fs.readFileSync('abiSunpad.json', 'utf8'));
+            const tokenContractABI  = JSON.parse(fs.readFileSync('abitoken.json', 'utf8'));
+            const routerSunPumpRouterAbi = JSON.parse(fs.readFileSync('abiSunPumpRouter.json', 'utf8'));
+            const sunPumpRouterCa = 'TZFs5ch1R1C4mmjwrrmZqeqbUgGpxY1yWB'
+            const sunPadLaunchpadCa = 'TTfvyrAz86hbZk5iDpKD78pqLGgi8C7AAw'
             
-            await saleToken(sunPadLaunchpadContract, tokenAddress, balanceToken);
+            let routerSunPumpContract = await tronWeb.contract(routerSunPumpRouterAbi, sunPumpRouterCa);
+            let sunPadLaunchpadContract = await tronWeb.contract(implementationABI, sunPadLaunchpadCa);
+            
+            console.log(`Address: ${address}\nBalance: ${Number(balanceInSun)/10**6} TRX\n`);
+            console.log('1. Swap TRX to Token\n2. Sale Token to TRX');
+            const choice = rl.question('Choice: ');
+    
+            if(choice == 1){
+                const tokenAddress = rl.question('Token Address: ');
+                const trxAmount = Number(rl.question('TRX Amount: '));
+
+                const isBonding = await checkBonding(routerSunPumpContract, tokenAddress);
+                if(!isBonding){
+                    await buyTokenInSunPump(sunPadLaunchpadContract, tokenAddress, (trxAmount*10**6));
+                }else{
+                    console.log('Token already bonding');
+                }
+            }else if(choice == 2){
+                const tokenAddress = rl.question('Token Address: ');
+                const tokenContract = await tronWeb.contract(tokenContractABI, tokenAddress);
+                const balanceToken = await getBalance(tokenContract, address);
+                console.log(`Balance Token: ${Number(balanceToken)/10**16}`);
+                
+                await saleToken(sunPadLaunchpadContract, tokenAddress, balanceToken);
+            }
+        } catch (error) {
+            console.error(error);
         }
-    } catch (error) {
-        console.error(error);
     }
     
 })()
